e4b93f9bc1639605431a461541feb6ef3b73049d
IkhwanAzhary <ikhwanazhary9@gmail.com>
2025-06-08 11:48:20 +0700

    Menambahkan Kelas Order

---
+++ CatatanStatusPesanan/Order.java
@@ -0,0 +1,34 @@
+package CatatanStatusPesanan;
+
+public class Order {
+	private int orderId;
+	private String customerName;
+	private OrderStatus status;
+
+	public Order(int orderId, String customerName) {
+		this.orderId = orderId;
+		this.customerName = customerName;
+		this.status = OrderStatus.DIPESAN;
+	}
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Pesanan #" + orderId + " - " + customerName + " [" + status + "]";
+	}
+}
